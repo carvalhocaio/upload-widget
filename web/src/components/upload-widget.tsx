e662155dac734ab70566344c488fb2cac82e064e
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-12-08 21:03:03 -0400

    feat: integrate Radix UI collapsible component and create button component

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -1,19 +1,30 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetDropList } from "./upload-widget-upload-list";
+import { useState } from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
-      <UploadWidgetHeader />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
+        <Collapsible.CollapsibleContent>
+          <UploadWidgetHeader />
 
-        <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
 
-        <UploadWidgetDropList />
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+
+            <UploadWidgetDropList />
+          </div>
+        </Collapsible.CollapsibleContent>
       </div>
-    </div>
+    </Collapsible.Root>
   )
 }
