738dda9878edafdc001fc56e74161550dc842de0
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-12-20 12:21:17 -0400

    feat: enhance upload management; add uploadId prop to UploadWidgetUploadItem, implement abort functionality, and update upload status handling

--- web/src/http/upload-file-to-storage.ts
+++ web/src/http/upload-file-to-storage.ts
@@ -4,14 +4,22 @@ interface uploadFileToStorageParams {
   file: File
 }
 
-export async function uploadFileToStorage({ file }: uploadFileToStorageParams) {
+interface UploadFileToStorageOpts {
+  signal?: AbortSignal
+}
+
+export async function uploadFileToStorage(
+  { file }: uploadFileToStorageParams,
+  opts?: UploadFileToStorageOpts
+) {
   const data = new FormData()
   data.append('file', file)
 
   const response = await axios.post<{ url: string }>('http://localhost:3333/uploads', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
-    }
+    },
+    signal: opts?.signal
   })
 
   return { url: response.data.url }
